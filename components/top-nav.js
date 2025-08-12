f38121113cc9cb33a44b9679904253e62336c981
wbf22 <brandon@fivefowlers.com>
2025-08-12 13:53:43 -0600

    Test for fixing links

--- components/top-nav.js
+++ components/top-nav.js
@@ -145,7 +145,7 @@ class TopNav extends HTMLElement {
             }
         </style>
         <header>
-            <a href="/glass_35/">
+            <a href="/">
                 <img id="top_logo" src="assets/glass_35_logo.png">
             </a>
             <div id='hamburger'>
@@ -154,11 +154,11 @@ class TopNav extends HTMLElement {
                 </svg>
             </div>
             <nav id='main_nav' class='main_nav_closed'>
-                <a class="nav_item spaced_title" href="/glass_35/">Home</a>
+                <a class="nav_item spaced_title" href="/">Home</a>
                 <a class="nav_item spaced_title" href="Services.html">Services</a>
                 <a class="nav_item spaced_title" href="About.html">About</a>
                 <div id="contact_nav">
-                    <a id="contact_nav_link" class="nav_item spaced_title" href="/glass_35/#contact">Contact</a>
+                    <a id="contact_nav_link" class="nav_item spaced_title" href="/#contact">Contact</a>
                     <div id="phone_section">
                         <svg xmlns="https://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
